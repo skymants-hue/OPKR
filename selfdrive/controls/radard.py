697415ab14eda2e788404d046b0c28f7fa3ea511
krcho1 <kiss7net@naver.com>
2025-05-18 14:07:32 +0900

    APP

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -226,12 +226,12 @@ def radard_thread(sm=None, pm=None, can_sock=None):
         if not msg:
           continue
         state = msg.get('STATE', 0)
-        if state in (3, 4):
-          azimuth = math.radians(msg['AZIMUTH'])
-          dist = msg['LONG_DIST']
-          x = math.cos(azimuth) * dist
-          y = -math.sin(azimuth) * dist
-          track_data.append((x, y, state))
+        #if state in (3, 4):
+        azimuth = math.radians(msg['AZIMUTH'])
+        dist = msg['LONG_DIST']
+        x = math.cos(azimuth) * dist
+        y = -math.sin(azimuth) * dist
+        track_data.append((x, y, state))
     # 저장 형식: "x,y,state;x,y,state;..."
     track_str = ';'.join(f"{x:.2f},{y:.2f},{state}" for x, y, state in track_data[:32])
     params.put("RadarTrackXY", track_str)
