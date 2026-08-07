379b7eacf45fceb762d1d42818df5bc0e73630e4
krcho1 <kiss7net@naver.com>
2026-08-07 21:28:16 +0900

    APP1

--- cereal/car.capnp
+++ cereal/car.capnp
@@ -251,6 +251,9 @@ struct CarState {
   doorOpen2 @58 :Bool;
   doorOpen3 @59 :Bool;
   doorOpen4 @60 :Bool;
+  lowLamp @61 :Bool;
+  fogLamp @62 :Bool;
+  highLamp @63 :Bool;
 
   struct TPMS {
     unit @0 :Int8;
--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -167,10 +167,14 @@ class CarState(CarStateBase):
     ret.doorOpen = any([cp.vl["CGW1"]["CF_Gway_DrvDrSw"], cp.vl["CGW1"]["CF_Gway_AstDrSw"],
                         cp.vl["CGW2"]["CF_Gway_RLDrSw"], cp.vl["CGW2"]["CF_Gway_RRDrSw"]])
 
+    # APP
     ret.doorOpen1 = cp.vl["CGW1"]["CF_Gway_DrvDrSw"] == 1
     ret.doorOpen2 = cp.vl["CGW1"]["CF_Gway_AstDrSw"] == 1
     ret.doorOpen3 = cp.vl["CGW2"]["CF_Gway_RLDrSw"] == 1
     ret.doorOpen4 = cp.vl["CGW2"]["CF_Gway_RRDrSw"] == 1
+    ret.lowLamp = cp.vl["CGW1"]["CF_Gway_HeadLampLow"] == 1
+    ret.fogLamp = cp.vl["CGW1"]["CF_Gway_Frt_Fog_Act"] == 1
+    ret.highLamp = cp.vl["CGW1"]["CF_Gway_HeadLampHigh"] == 1
 
     ret.seatbeltUnlatched = cp.vl["CGW1"]["CF_Gway_DrvSeatBeltSw"] == 0
 
