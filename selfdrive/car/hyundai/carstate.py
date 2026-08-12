3744bee490932ab8d7055a1bdfccb5b36e90dcc4
krcho1 <kiss7net@naver.com>
2026-08-12 21:47:04 +0900

    APP1

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -179,16 +179,16 @@ class CarState(CarStateBase):
     ret.pBrakeAct = cp.vl["TCS13"]["PBRAKE_ACT"] == 1
     ret.astSeatBeltSw = cp.vl["CGW1"]["CF_Gway_AstSeatBeltSw"] == 0
     ret.trunkTgSw = cp.vl["CGW1"]["CF_Gway_TrunkTgSw"]
-    if self.CP.sccBus == 1:
+    if self.CP.sccBus in (1,2):
       tracks = ret.initRadarTracks(32)
 
-    for i in range(0x500, 0x520):
-      idx = i - 0x500
-      msg = f"RADAR_TRACK_{i:03x}"
+      for i in range(0x500, 0x520):
+        idx = i - 0x500
+        msg = f"RADAR_TRACK_{i:03x}"
 
-      tracks[idx].azi = cp2.vl[msg]["AZIMUTH"]
-      tracks[idx].stat = cp2.vl[msg]["STATE"]
-      tracks[idx].dist = cp2.vl[msg]["LONG_DIST"]
+        tracks[idx].azi = cp_scc.vl[msg]["AZIMUTH"]
+        tracks[idx].stat = cp_scc.vl[msg]["STATE"]
+        tracks[idx].dist = cp_scc.vl[msg]["LONG_DIST"]
 
 
     ret.seatbeltUnlatched = cp.vl["CGW1"]["CF_Gway_DrvSeatBeltSw"] == 0
@@ -672,14 +672,15 @@ class CarState(CarStateBase):
     signals = []
     checks = []
     #APP
-    for i in range(0x500, 0x520):
-      msg = f"RADAR_TRACK_{i:03x}"
-      signals.extend([
-        ("AZIMUTH", msg),
-        ("STATE", msg),
-        ("LONG_DIST", msg),
-      ])
-      #checks.append((msg, 100))
+    if CP.sccBus == 1:
+      for i in range(0x500, 0x520):
+        msg = f"RADAR_TRACK_{i:03x}"
+        signals.extend([
+          ("AZIMUTH", msg),
+          ("STATE", msg),
+          ("LONG_DIST", msg),
+        ])
+        #checks.append((msg, 100))
 
     if CP.mdpsBus == 1:
       signals += [
@@ -781,6 +782,18 @@ class CarState(CarStateBase):
       ("CF_Lkas_LdwsOpt_USM", "LKAS11")
     ]
     checks = [("LKAS11", 100)]
+
+    #APP
+    if CP.sccBus == 2:
+      for i in range(0x500, 0x520):
+        msg = f"RADAR_TRACK_{i:03x}"
+        signals.extend([
+          ("AZIMUTH", msg),
+          ("STATE", msg),
+          ("LONG_DIST", msg),
+        ])
+        #checks.append((msg, 100))
+
     if CP.sccBus == 2:
       signals += [
         ("MainMode_ACC", "SCC11"),
